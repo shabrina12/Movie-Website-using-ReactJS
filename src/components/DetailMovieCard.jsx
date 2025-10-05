63087da037e74306f5643a63c936771c02deb338
Shabrina Putri <shabbrina5@gmail.com>
2025-10-05 19:17:46 +0700

    add styling for responsive modal pop up

--- src/components/DetailMovieCard.jsx
+++ src/components/DetailMovieCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const DetailMovieCard = ({title, overview, vote_average, backdrop_path}) => {
   return (
-        <div className="fixed top-[20%] left-1/2 -translate-x-1/2 flex flex-col items-center justify-center px-12 rounded-lg w-210 h-136 text-center z-60">
+        <div className="fixed xs:top-[30%] md:top-[20%] left-1/2 -translate-x-1/2 flex flex-col items-center justify-center xs:px-4 md:px-8 lg:px-12 rounded-lg xs:w-100 xs:h-90 md:w-140 md:h-120 lg:w-180 lg:h-116 xl:w-210 xl:h-136 text-center z-60">
             {/* Background image */}
             <img
               src={backdrop_path ? 
@@ -14,13 +14,13 @@ const DetailMovieCard = ({title, overview, vote_average, backdrop_path}) => {
             <div className="absolute inset-0 bg-black/60 rounded-lg"></div>
             
             <div className='relative z-70 flex flex-col items-center text-white'>
-              <h2 className="text-2xl font-bold mb-2">{title} </h2>
+              <h2 className="xs:text-xl md:text-2xl lg:text-3xl font-bold mb-2">{title} </h2>
               <div className='flex gap-2 items-center'>
-                <h2 className='text-xl'>{vote_average ? vote_average.toFixed(1) : 'N/A'}</h2>
+                <h2 className='xs:text-lg md:text-xl lg:text-2xl'>{vote_average ? vote_average.toFixed(1) : 'N/A'}</h2>
                 <img className='size-5 object-contain' src='star.svg' alt='star icon' />
               </div>              
               
-              <p className="text-xl text-white mt-4">{overview}</p>
+              <p className="xs:text-md md:text-lg lg:text-xl text-white mt-4">{overview}</p>
             </div>
             
         </div>      
